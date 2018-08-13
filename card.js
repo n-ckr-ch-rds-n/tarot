c4e195903b6fa8221e1e86c2fbb30aacdfd565d5
Nick <nicholasjohnrichardson@gmail.com>
2018-08-13 19:12:21 +0100

    game loads cards

--- card.js
+++ card.js
@@ -5,18 +5,27 @@ const e = React.createElement;
 class Card extends React.Component {
   constructor(props) {
     super(props);
-    this.state = { flipped: false };
+    this.state = {
+      flipped: false,
+      cards: ['chariot.jpg', 'death.jpg', 'devil.jpg', 'emperor.png', 'empress.png', 'fool.jpg', 'hanged_man.jpg', 'heirophant.png', 'hermit.jpg', 'high_priestess.png', 'judgment.jpg', 'justice.jpg', 'lovers.jpg', 'magician.png', 'star.jpg', 'sun.jpg', 'temperance.jpg', 'tower.jpg', 'wheel_of_fortune.jpg', 'world.jpg']
+    };
   }
 
   render() {
     if (this.state.flipped) {
-      return 'Card' + this.props.cardID + ' is flipped';
+      return e(
+        'img', {
+          src: `./trumps/${this.state.cards[Math.floor(Math.random() * 21)]}`,
+          onClick: () => this.setState({ flipped: true })
+        }
+      );
     }
 
     return e(
-      'button',
-      { onClick: () => this.setState({ flipped: true })},
-      'Flip'
+      'img', {
+        src: './images/tarot_back.jpg',
+        onClick: () => this.setState({ flipped: true })
+      },
     );
   }
 }
